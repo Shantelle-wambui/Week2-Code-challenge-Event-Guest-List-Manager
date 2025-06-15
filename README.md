ae59e3d4099f3b9d2df7ebea9da6ff6eed5b3558
shantelle wambui <shantellewambuikungu@gmail.com>
2025-06-15 21:34:06 +0300

    Initial commit: Event Guest List Manager

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Week2-Code-challenge-Event-Guest-List-Manager
+# 🎉 Event Guest List Manager
+
+A simple and interactive web app to manage an event guest list. Built using **HTML**, **CSS**, and **JavaScript**, this tool lets you add, remove, and track RSVPs with smooth animations and an easy-to-use interface.
+
+---
+
+## ✨ Features
+
+- ✅ Add guest names to the list
+- ✅ Limit guest list to 10 people
+- ✅ Mark guests as “Attending” or “Not Attending” (RSVP toggle)
+- ✅ Remove guests from the list
+- ✅ Smooth CSS animations when adding/removing guests
+- ✅ Form resets and refocuses automatically after adding a guest
+
+---
+
+## 🚀 How to Run the Project
+Git clone https://github.com/Shantelle-wambui/Week2-Code-challenge-Event-Guest-List-Manager.git
+Simply open index.html in your browser.
+Start typing guest names and click “Add Guest”.
+
+## 🙌 Author
+Built with ❤️ by Shantelle Wambui Kungu
+Git hub account-Shantelle wambui
+
---
+++ guest.js
@@ -0,0 +1,60 @@
+const guestForm=document.getElementById('guestForm');
+const guestInput=document.getElementById('guestInput');
+const  guestList=document.getElementById('guestList');
+//get the form,input box and list from the html using their ids
+
+
+guestForm.addEventListener('submit', function(e) {
+    e.preventDefault();//stop the page from reloading
+    const name=guestInput.ariaValueMax.trim();//get the name typed by the user,and remove extra spaces
+    if(name==='')return;
+//check if there are already 10 guests in the list
+    if(guestList.children.length>=10){
+        alert("Guest list is full! Maximum 10 guest allowed");
+        return;//stop adding if we have 10 guests already
+
+    }
+    addGuest(name);//if everything is okay , add the guest
+    guestInput.value='';
+    guestInput.focus();
+    //clears the input box and focus the cursor back in the input
+});
+
+//this function creates and adds a new guest item to the list
+function addGuest(name){
+    const li=document.createElement('li');//create a new <Li>element(list item)
+    const nameSpan=document.createElement('span');
+
+    nameSpan.textContent=name;//set the name inside the span
+
+    //RSVP button
+    const rsvpBtn=document.createElement('button')
+    rsvpBtn.textContent='Attending';//default text
+    rsvpBtn.className='btn rsvp-btn';
+
+    rsvpBtn.addEventListener('click', ()=>{//when rsvp button is clicked , toggle the text and background color
+        if(rsvpBtn.textContent==='Attending'){
+            rsvpBtn.textContent='Not Attending';
+            rsvpBtn.style.backgroundColor='#cc'
+        }else {
+            rsvpBtn.textContent='Attending';
+            rsvpBtn.style.backgroundColor='#3498db'
+        }
+    });
+
+    //remove button
+    const removeBtn=document.createElement('button');
+    removeBtn.textContent='Remove';
+    removeBtn.className='btn remove-btn'
+    removeBtn.addEventListener('click', ()=> //when remove is clicked, remove this guest from the list
+        {guestList.removeChild(li);
+});
+//add the guest name, rsvp button and remove button into the<li>
+    li.appendChild(nameSpan);
+    li.appendChild(rsvpBtn);
+    li.appendChild(removeBtn);
+
+    guestList.appendChild(li);//add the<li> to the list shown on the page
+
+
+}
